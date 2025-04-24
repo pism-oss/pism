7dae42ed5bc6775dbf1b7ba7bd8e28c9b621e2cd
PerccyKing <perccyking@gmail.com>
2025-04-24 22:45:20 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,6 +38,14 @@ const config: Config = {
         name: 'google-adsense-account',
         content: 'ca-pub-7006111376650963'
       }
+    },
+    {
+      tagName: 'script',
+      attributes: {
+        async: true,
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7006111376650963',
+        crossorigin: 'anonymous'
+      }
     }
   ],
 
