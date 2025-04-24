55f0bfbcedc8c420e945c3565aacd962f7503022
PerccyKing <perccyking@gmail.com>
2025-04-24 22:51:32 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
     {
       tagName: 'script',
       attributes: {
-        async: true,
+        async: "async",
         src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7006111376650963',
         crossorigin: 'anonymous'
       }
